3729f3e78480702376c34eec6bfe5b2a34fa05ea
nminekshi <nminekshi@gmail.com>
2025-03-17 13:30:54 +0530

    init

---
+++ app/Registration/page.tsx
@@ -0,0 +1,11 @@
+import RegistrationForm from '../component/Registration';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
